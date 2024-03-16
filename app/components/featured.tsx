11cbc7973b3b0219bec37ebded7d15e23dbbcdb7
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-03-16 11:31:13 +0800

    misc

--- app/components/featured.tsx
+++ app/components/featured.tsx
@@ -2,11 +2,11 @@ import { FaCode, FaGlobe, FaAndroid } from "react-icons/fa";
 
 export default function Featured() {
     return (
-        <div className="mx-7 sm:mx-10 my-36 relative z-20">
+        <div className="mx-7 sm:mx-10 my- md:my-36 relative z-20">
             <h1 className="text-3xl sm:text-4xl font-semibold">Featured Projects</h1>
             <div className="flex flex-col md:grid sm:justify-between mt-14 md:grid-cols-3 gap-3.5 sm:gap-0.5 mt-14 md:gap-3">
                 <a href="https://github.com/brentibanez331/plant-disease-classification" target="_blank">
-                    <div className="bg-neutral-950 mb-3.5 md:h-52 p-6 rounded-md flex flex-col justify-between transition duration-300 hover:scale-105 w-full">
+                    <div className="bg-neutral-900 mb-3.5 md:h-52 p-6 rounded-md flex flex-col justify-between transition duration-300 hover:scale-105 w-full">
                         <div>
                             <h2 className="text-lg font-bold mb-3.5">plant-disease-classification</h2>
                             <p className="text-sm">A computer vision model for predicting plant diseases. Deployed web app, and mobile app in production.</p>
