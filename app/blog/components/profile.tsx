9765b4889c08f52508e60738e43a9e1cd0cb3c22
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-03-18 21:43:45 +0800

    feat: added blog page

--- app/blog/components/profile.tsx
+++ app/blog/components/profile.tsx
@@ -4,13 +4,13 @@ import { IoIosArrowRoundBack } from "react-icons/io";
 
 export default function Profile() {
     return (
-        <div className="mx-6 pt-16">
-            <Link href="/" className="w-7 z-50 ml-7"><IoIosArrowRoundBack className="text-5xl"/></Link>
+        <div className="mx-6 pt-16 sm:mx-10 lg:mx-40 2xl:mx-80 lg:mb-14 lg:pt-7">
+            <Link href="/" className="w-7 z-50 ml-7"><IoIosArrowRoundBack className="text-5xl hover:-translate-x-1.5 transition ease-in-out duration-300"/></Link>
             <div className="flex items-center pt-32">
-                <Image src="/brent-2.jpg" width={500} height={500} className="mr-5 rounded-full w-28 h-28 object-cover" alt="Brent Ibañez" />
+                <Image src="/brent-2.jpg" width={500} height={500} className="mr-5 lg:mr-8 rounded-full size-28 object-cover lg:size-32" alt="Brent Ibañez" />
                 <div className="">
-                    <h1 className="font-bold text-2xl">Brent Ibañez</h1>
-                    <h1 className="">@renibanez</h1>
+                    <h1 className="font-bold text-2xl lg:text-3xl">Brent Ibañez</h1>
+                    <h1 className="lg:text-xl">@renibanez</h1>
                 </div>
             </div>
         </div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,7 @@ import Profile from "./components/profile";
 import Content from "./components/content";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import Link from "next/link";
+import Footer from "../components/footer";
 
 export default function Blog() {
   const [isLoading, setIsLoading] = useState(true);
@@ -45,9 +46,9 @@ export default function Blog() {
           </div>
         </div>
       )}
-      <div className="relative h-screen">
-        <div className="h-screen w-full absolute">
-          <div className="bg-gradient-to-b from-transparent from-10% to-[#0a0a0c] to-40% w-full h-full relative z-10"></div>
+      <div className="relative h-screen xl:h-full">
+        <div className="h-screen w-full absolute xl:h-full">
+          <div className="bg-gradient-to-b from-transparent from-10% to-[#0a0a0c] to-40% w-full h-full relative z-10 xl:to-[32%]"></div>
           <Image src="/8.png" width={3000} height={3000} alt="My SVG" className="-z-10 h-96 sm:h-auto object-cover sm:w-full absolute top-0"></Image>
         </div>
         <div>
@@ -56,6 +57,7 @@ export default function Blog() {
         <div className="relative z-40">
           <Profile></Profile>
           <Content></Content>
+          <Footer></Footer>
         </div>
       </div>
 
